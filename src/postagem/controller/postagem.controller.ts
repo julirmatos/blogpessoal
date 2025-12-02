d0779fe43ea4a47bb704a15cdab72a7d97b57b84
julirmatos <juli.rmatos@gmail.com>
2025-12-02 15:28:18 -0300

    Blog Pessoal

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,5 +1,6 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { DeleteResult } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
 import { PostagemService } from '../services/postagem.service';
 
@@ -16,6 +17,30 @@ export class PostagemController {
     @HttpCode(HttpStatus.OK)
     findById(@Param('id', ParseIntPipe) id: number): Promise<Postagem> {
         return this.postagemService.findById(id);
+    }
+
+    @Get('/titulo/:titulo') // buscar postagem por titulo
+    @HttpCode(HttpStatus.OK)
+    findAllByTitulo(@Param('titulo') titulo: string): Promise<Postagem[]> {
+        return this.postagemService.findAllByTitulo(titulo);
+    }
+
+    @Post() // criar uma nova postagem
+    @HttpCode(HttpStatus.CREATED)
+    createPostagem(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.createPostagem(postagem);
+    }
+
+    @Put() // atualizar uma postagem
+    @HttpCode(HttpStatus.OK)
+    updatePostagem(@Body() postagem: Postagem): Promise<Postagem> {
+        return this.postagemService.updatePostagem(postagem);
+    }
+
+    @Delete('/:id') // deletar uma postagem
+    @HttpCode(HttpStatus.NO_CONTENT)
+    deletePostagem(@Param('id', ParseIntPipe) id: number): Promise<DeleteResult> {
+        return this.postagemService.deletePostagem(id);
     }
 }
 
--- src/postagem/services/postagem.service.ts
+++ src/postagem/services/postagem.service.ts
@@ -1,7 +1,7 @@
 /* eslint-disable prettier/prettier */
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, ILike, Repository } from 'typeorm';
 import { Postagem } from '../entities/postagem.entity';
 
 @Injectable() // classe pode ser inserida em outras classes
@@ -22,5 +22,27 @@ export class PostagemService {
             throw new HttpException('Postagem não Encontrada', HttpStatus.NOT_FOUND);
         }
         return postagem;
+    }
+
+    async findAllByTitulo(titulo: string): Promise<Postagem[]> {
+        return await this.postagemRepository.find({
+            where: {
+                titulo: ILike(`%${titulo}%`)
+            }
+        });
+    }
+
+    async createPostagem(postagem: Postagem): Promise<Postagem> {
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async updatePostagem(postagem: Postagem): Promise<Postagem> {
+        await this.findById(postagem.id);
+        return await this.postagemRepository.save(postagem);
+    }
+
+    async deletePostagem(id: number): Promise<DeleteResult> {
+        await this.findById(id);
+        return await this.postagemRepository.delete(id);
     }
 }
