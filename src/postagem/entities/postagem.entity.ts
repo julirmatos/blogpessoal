b3e92f8e7582088d2b066ab6b2d7a939c3e8b4d5
Juliana Matos <106282913+julirmatos@users.noreply.github.com>
2026-01-21 10:16:48 -0300

    Update postagem

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty } from "class-validator";
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
-import { Tema } from "../../temas/entities/tema.entity";
+import { Tema } from "../../tema/entities/tema.entity";
 import { Usuario } from "../../usuario/entities/usuario.entity";
 import { ApiProperty } from "@nestjs/swagger";
 
