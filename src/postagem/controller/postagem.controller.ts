dd0e85d1629de74dc7b5c82c51816f343c20a2e7
Juliana Matos <106282913+julirmatos@users.noreply.github.com>
2026-01-21 10:02:10 -0300

    Update postagem

--- src/postagem/controller/postagem.controller.ts
+++ src/postagem/controller/postagem.controller.ts
@@ -1,15 +1,14 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
-import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { PostagemService } from "../services/postagem.service";
 import { Postagem } from "../entities/postagem.entity";
 import { DeleteResult } from "typeorm";
 import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 
+@UseGuards(JwtAuthGuard)     // Colocando essa Anotação aqui, indica que todos os endpoints são protegidos
+@Controller("/postagens")   // Indica que a Classe é uma Controller
 @ApiTags('Postagem')
-@UseGuards(JwtAuthGuard)
-@Controller("/postagens")
 @ApiBearerAuth()
-  // Indica que a Classe é uma Controller
 export class PostagemController {
 
     // Dentro do Construtor injetamos o postagemService para podermos usar seus métodos
@@ -41,7 +40,7 @@ export class PostagemController {
     @Post() // Usado quando queremos Cadastrar/Criar/Salva alguma informação
     @HttpCode(HttpStatus.CREATED)   // Monta a Resposa HTTP para o Cliente com o status 201
     create(@Body() postagem: Postagem): Promise<Postagem> {
-        return this.postagemService.createPostagem(postagem);
+        return this.postagemService.create(postagem);
     }
 
     // @Put() Indica que esse método lida com Requisições do Tipo Put
@@ -49,7 +48,7 @@ export class PostagemController {
     @Put()// Usado quando queremos Atualizar alguma informação
     @HttpCode(HttpStatus.OK) // Monta a Resposa HTTP para o Cliente com o status 200    
     update(@Body() postagem: Postagem): Promise<Postagem> {
-        return this.postagemService.updatePostagem(postagem.id, postagem);
+        return this.postagemService.update(postagem);
     }
 
     // @Delete('/:ID') Indica que esse método lida com Requisições do Tipo DELETE e que no seu endpoint será enviado um id como parametro
