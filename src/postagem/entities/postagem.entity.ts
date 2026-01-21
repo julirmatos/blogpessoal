6cb7388190d0112e5b3280ca39ee22e9f4b33fdd
Juliana Matos <106282913+julirmatos@users.noreply.github.com>
2026-01-21 10:01:13 -0300

    Update postagem

--- src/postagem/entities/postagem.entity.ts
+++ src/postagem/entities/postagem.entity.ts
@@ -1,44 +1,42 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
+import { Tema } from "../../temas/entities/tema.entity";
+import { Usuario } from "../../usuario/entities/usuario.entity";
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty } from 'class-validator';
-import {
-    Column,
-    Entity,
-    ManyToOne,
-    PrimaryGeneratedColumn,
-    UpdateDateColumn,
-} from 'typeorm';
-import { Tema } from '../../tema/entities/tema.entity';
-import { Usuario } from '../../usuario/entities/usuario.entity';
 
-@Entity({ name: 'tb_postagens' })
+@Entity({ name: "tb_postagens" })   // Indicando que a classe é uma Entitidade/Model
 export class Postagem {
+
     @ApiProperty()
-    @PrimaryGeneratedColumn()
+    @PrimaryGeneratedColumn()   // Chave Primária e Auto Incremental
     id: number;
 
     @ApiProperty()
-    @IsNotEmpty()
-    @Column({ length: 100, nullable: false })
+    @IsNotEmpty()   // Decorator usado para como Validador de Objetos no corpo da Requisição
+    @Column({ length: 100, nullable: false })   // Tamanho Máximo: 100 | Regra do MySQL - NOT NULL
     titulo: string;
 
     @ApiProperty()
-    @IsNotEmpty()
-    @Column({ length: 1000, nullable: false })
+    @IsNotEmpty()   // Validador de Objeto
+    @Column({ length: 1000, nullable: false })   // Tamanho Máximo: 1000 | Regra do MySQL - NOT NULL
     texto: string;
 
     @ApiProperty()
-    @UpdateDateColumn()
+    @UpdateDateColumn() // Indica que o campo será gerenciado pelo BD
     data: Date;
 
-    @ApiProperty()
+    // Indica o lado MUITO do relacionamento, indicando que esse campo se conecta ao campo Postagem da Model Tema
     @ManyToOne(() => Tema, (tema) => tema.postagem, {
-        onDelete: 'CASCADE',
+        onDelete: "CASCADE"
     })
-    tema: Tema;
-    @ApiProperty()
+    @ApiProperty({ type: () => Tema })
+    tema: Tema
+
+    // Indica o lado MUITO do relacionamento, indicando que esse campo se conecta ao campo Postagem da Model Usuario
     @ManyToOne(() => Usuario, (usuario) => usuario.postagem, {
         onDelete: "CASCADE"
     })
-    usuario: Usuario;
+    @ApiProperty({ type: () => Usuario })
+    usuario: Usuario
 
 }
